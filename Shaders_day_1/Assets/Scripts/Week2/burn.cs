96b769af7fa7f0a17e060e9fed1792d61a524ebd
George Wulfers <george.wulfers@gmail.com>
2016-10-15 02:57:21 -0700

    Shaders Homework
    Completed Shaders homework. You can move the plane anywhere in the world
    and click on the plane and it will convert the hit point from world
    position into UV coordinates.

--- Shaders_day_1/Assets/Scripts/Week2/burn.cs
+++ Shaders_day_1/Assets/Scripts/Week2/burn.cs
@@ -8,12 +8,14 @@ public class burn : MonoBehaviour
     Material burnMaterial;
     float Threshold1;
     float Threshold2;
+    Vector2 WorldToUV;
+    bool Burning = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         burnMaterial = GetComponent<Renderer>().material;
-        if(burnMaterial.shader.name != "Custom/burn")
+        if(burnMaterial.shader.name != "Custom/ClickBurn")
         {
             Destroy(this);
         }
@@ -25,11 +27,50 @@ public class burn : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        Threshold2 += (Time.deltaTime * rate);
-        Threshold1 += (Time.deltaTime * rate) * 0.5f;
+        Vector2 MouseP = Vector2.zero;
+        GameObject obj = null;
+        RaycastHit hit;
+        Ray ray;
 
-        Threshold1 = Mathf.Clamp01(Threshold1);
-        Threshold2 = Mathf.Clamp01(Threshold2);
+        if (Input.GetMouseButtonDown(0) && !Burning)
+        {
+            Burning = true;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject)
+                {
+                    obj = hit.collider.gameObject;
+                    MouseP = hit.point;
+                }
+            }
+        }
+        
+        if (Burning)
+        {
+            if(obj)
+            {
+                Vector2 Dims = obj.GetComponent<Collider>().bounds.size;
+                Vector2 RelativeCenter = new Vector2(0.5f, 0.5f);
+                WorldToUV.x = Mathf.Abs((transform.position.x - RelativeCenter.x) - MouseP.x / Dims.x);
+                WorldToUV.y = Mathf.Abs((transform.position.y - RelativeCenter.y) - MouseP.y / Dims.y);
+            }
+            
+            Threshold2 += (Time.deltaTime * rate);
+            Threshold1 += (Time.deltaTime * rate) * 0.85f;
+
+            Threshold1 = Mathf.Clamp(Threshold1, 0.0f, 0.69f);
+            Threshold2 = Mathf.Clamp(Threshold2, 0.0f, 0.69f);
+
+            if (Threshold1 >= 0.68f && Threshold2 >= 0.68f)
+            {
+                Threshold2 = Threshold1 = 0;
+                Burning = false;
+            }
+        }
+        
+        burnMaterial.SetFloat("_MouseX", WorldToUV.x);
+        burnMaterial.SetFloat("_MouseY", WorldToUV.y);
 
         burnMaterial.SetFloat("_Thresh1", Threshold1);
         burnMaterial.SetFloat("_Thresh2", Threshold2);
